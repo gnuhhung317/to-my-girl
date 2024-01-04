baedf2faefe99c7ee65df1b74c2f28bd8c5f3b0e
Duc Hung <duchung04st@gmail.com>
2024-01-04 21:03:17 +0700

    init

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Em ăn cơm chưa`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em nhớ anh suốt mấy ngày vừa qua 😙',
+    desc: 'Còn anh thì nhớ em suốt mấy ngày rồi đấy ',
+    btnYes: 'Vẫn cứ là nhớ anh <33',
+    btnNo: 'Không, đéo ;<',
+    question:'Trên thế giới hơn 7 tỉ người mà anh không tốt thế sao mà em vẫn yêu😓',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bởi vì em quá xinh đẹp. Quá đỗi yêu kiều <3333😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình gặp nhauuu nhasaaaa><.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100038151938164' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
